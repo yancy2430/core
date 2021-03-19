094c13ab5e4b3435059569a5631ae2912cda5215
杨哲 <2850755733@qq.com>
2021-03-19 21:15:41 +0800

    添加邮箱发送服务

--- src/main/java/com/tdeado/core/config/OkhttpConfig.java
+++ src/main/java/com/tdeado/core/config/OkhttpConfig.java
@@ -1,6 +1,8 @@
 package com.tdeado.core.config;
 
 import okhttp3.OkHttpClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/tdeado/core/config/OssConfig.java
+++ src/main/java/com/tdeado/core/config/OssConfig.java
@@ -4,6 +4,7 @@ import com.aliyun.oss.OSSClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class OssConfig {
@@ -12,6 +13,9 @@ public class OssConfig {
 
     @Bean
     public OSSClient OSSClient(){
+        if (StringUtils.isEmpty(uploadFileProperties.getOssAccessKeyId())){
+            return null;
+        }
         OSSClient ossClient = new OSSClient(uploadFileProperties.getOssEndpoint(), uploadFileProperties.getOssAccessKeyId(), uploadFileProperties.getOssAccessKeySecret());
         return ossClient;
     }
